0c536ba0b82cee646c2bc2d1d219118ce6e4c05d
Kate <kate.matveeva@gmail.com>
2021-04-13 01:17:24 +0300

    feat: add solution for minesweeper function

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,43 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const newMat = [];
+  for (let i = 0; i < matrix.length; i++) {
+    newMat.push(matrix[i].slice());
+  }
+  newMat.forEach((row) => row.fill(0));
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j]) {
+        if (i > 0 && j > 0) {
+          newMat[i - 1][j - 1] += 1;
+        }
+        if (i > 0) {
+          newMat[i - 1][j] += 1;
+        }
+        if (i > 0 && j < matrix[0].length - 1) {
+          newMat[i - 1][j + 1] += 1;
+        }
+        //
+        if (j > 0) {
+          newMat[i][j - 1] += 1;
+        }
+        if (j < matrix[0].length - 1) {
+          newMat[i][j + 1] += 1;
+        }
+        if (i < matrix.length && j > 0) {
+          newMat[i + 1][j - 1] += 1;
+        }
+        if (i < matrix.length) {
+          newMat[i + 1][j] += 1;
+        }
+        if (i < matrix.length && j < matrix[0].length - 1) {
+          newMat[i + 1][j + 1] += 1;
+        }
+      }
+    }
+  }
+  return newMat;
 }
-
 module.exports = minesweeper;
